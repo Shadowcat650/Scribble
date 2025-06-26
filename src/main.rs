f9a5e12735f7ffbc9424fa643560f50234a177b5
ShadowCat650 <64231061+Shadowcat650@users.noreply.github.com>
2025-06-26 13:06:05 -0700

    anger clippy

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
 fn main(
     
 ) {
-    println!("Hello, world!");
+    print('H', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd', '!');
+}
+
+fn print(
+    a: char,
+    b: char,
+    c: char,
+    d: char,
+    e: char,
+    f: char,
+    g: char,
+    h: char,
+    i: char,
+    j: char,
+    k: char,
+    l: char,
+    m: char,
+) {
+    println!("{a}{b}{c}{d}{e}{f}{g}{h}{i}{j}{k}{l}{m}");
 }
 
 #[cfg(test)]
